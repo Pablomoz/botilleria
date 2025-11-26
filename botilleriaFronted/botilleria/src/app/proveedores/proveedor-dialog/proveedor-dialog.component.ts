f9df49fffd3e16fdfef6ccb3532a7c305ff3d82b
DESKTOP-TKBDNGR\pablomoz <pablomoz@hotmail.com>
2025-11-26 15:50:42 -0300

    dos

---
+++ botilleriaFronted/botilleria/src/app/proveedores/proveedor-dialog/proveedor-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedor-dialog',
+  imports: [],
+  templateUrl: './proveedor-dialog.component.html',
+  styleUrl: './proveedor-dialog.component.css'
+})
+export class ProveedorDialogComponent {
+
+}
